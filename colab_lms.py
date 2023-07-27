5a7d08412265b3c861fdfb69991ea94db2a344a4
Maite Madalosso <48294273+MFMG99@users.noreply.github.com>
2023-07-27 00:47:13 -0400

    Update url cases

--- colab_lms.py
+++ colab_lms.py
@@ -1,6 +1,6 @@
 global DEBUG_LOCAL
 DEBUG_LOCAL = False
-PATH_CASES = 'https://raw.githubusercontent.com/rilopez3/colab_lms/main_run_tests/cases_urls.txt'
+PATH_CASES = 'https://raw.githubusercontent.com/rilopez3/colab_lms/guia2_2023/cases_urls.txt'
 
 ###################################################################################################################################
 
