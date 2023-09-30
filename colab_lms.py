9a355ac83adf90a4bdd848483183309db21bfd0b
fddelabarra <fddelabarra@uc.cl>
2023-09-30 20:59:52 -0300

    links update

--- colab_lms.py
+++ colab_lms.py
@@ -1,7 +1,7 @@
 import diff_match_patch as dmp_module
 global DEBUG_LOCAL
 DEBUG_LOCAL = False
-PATH_CASES = 'https://raw.githubusercontent.com/rilopez3/colab_lms/guia6_2023/cases_urls.txt'
+PATH_CASES = 'https://raw.githubusercontent.com/rilopez3/colab_lms/guia7_2023/cases_urls.txt'
 
 ###################################################################################################################################
 import io, sys, traceback, hashlib, urllib.request
