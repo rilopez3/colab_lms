93d7e9f0153358e845cf8bf1055fa03c84c63c5f
Rodrigo I. López <69801971+rilopez3@users.noreply.github.com>
2023-11-22 16:53:42 -0300

    eval responses

--- colab_lms_eval.py
+++ colab_lms_eval.py
@@ -1,89 +1,198 @@
-import io, sys, traceback, hashlib, urllib.request
+import traceback
+import urllib.request
+import hashlib
+import threading
 
-def get_case(preg):
+
+def csv_format( data:list):
+  return ','.join([ str(i) for i in data])
+
+# Obtiene url de los cases
+def get_case_path(preg):
+  global PATH_CASES
   cases = {}
-  with open('cases_urls.txt','r') as f:
-    for l in f.readlines():
-      l = l.strip().split(',')
-      cases[l[0]] = l[1]
+  f = urllib.request.urlopen(PATH_CASES)
+  for l in f.readlines():
+    l = l.decode("utf-8")
+    l = l.strip().split(',')
+    cases[l[0]] = l[1]
   if preg in cases:
     return cases[preg]
   return None
 
-def clean_exec():
-  var_clean = evaluation_variables()
-  clean_text = ''
-  for i in var_clean:
-    clean_text += i + '= None\n'
-  return clean_text
+# Lee un archivo de case y retorna case en formato de diccionario
+def read_cases(fileurl):
+  cases = []
+  if fileurl:
+    try:
+      f = urllib.request.urlopen(fileurl)
+      lineas = []
+      for l in f.readlines():
+        l = l.decode("utf-8")
+        lineas.append(l)
+      cases = parse_case(lineas)
+    except Exception as e:
+      print(traceback.format_exc())
+  return cases
 
-def eval_preg(preg):
-  nombre, cases = read_cases(get_case(preg))
-  pub = 0
-  t_pub = 0
-  sec = 0
-  t_sec = 0
-  obs = ''
-  if cases:
-    for i, val in enumerate(cases):
-      subname, tipo, code, res = val
-      if tipo == 'Publico':
-        point, comentario = eval_case(subname, tipo, code, res)
-        pub += point
-        if comentario not in obs:
-          obs += comentario
-        t_pub += 1
-      elif tipo == 'Secreto':
-        point, comentario = eval_case(subname, tipo, code, res)
-        sec += point
-        if comentario not in obs:
-          obs += comentario
-        t_sec += 1
-    return [pub, t_pub, sec, t_sec, obs]
-  return None
+# Limpia caracteres unicode
+def clean_code(student_code):
+  to_clean = [
+    ['\xa0',' '],
+  ]
+  for row in to_clean:
+    student_code = student_code.replace(row[0], row[1])
+  return student_code
 
-def evaluation_variables():
-  return ['nombre','correo_escuela_militar','seccion']
+# Obtiene parse de un archivo de cases
+def parse_case(lineas):
+  data = {'PREG':'', 'CASES':[]}
+  template = {'NAME':'', 'TYPE':'', 'PRECODE': '', 'INPUT': '', 'CODE': '', 'OUTPUT': ''}
+  caso_actual  = None
+  for linea in lineas:
+    if linea.startswith('%'):
+      clave_actual = linea[1:].strip()
+      if clave_actual == 'NAME':
+        if caso_actual is not None:
+          data['CASES'].append(caso_actual)
+        caso_actual = template.copy()
+    else:
+      if clave_actual == 'PREG':
+        data['PREG'] += linea
+      else:
+        caso_actual[clave_actual] += linea
+  if caso_actual is not None:
+    data['CASES'].append(caso_actual)
+  data['PREG'] = data['PREG'].strip()
+  for case in data['CASES']:
+    case['NAME'] = case['NAME'].strip()
+    case['TYPE'] = case['TYPE'].strip()
+  return data
 
-def get_variables():
-  var_to_get = evaluation_variables()
-  
-  var_code = 'data = {\'vars\':['
-  for i in var_to_get:
-    var_code += str(i) + ','
-  var_code += ']}'
+# Nueva funcion de print que reemplaza a la de los alumnos
+def new_print(*args, sep = " ", end = "\n"):
+  global STUDENT_OUTPUT_CONTROL
+  output = sep.join([ push_str(val) for val in args]) + end
+  STUDENT_OUTPUT_CONTROL += output
 
-  return var_code
+# Conversor dinamico a formato str
+def push_str(val):
+  if type(val) == type(1):
+    return str(val)
+  if type(val) == type(0.5):
+    return str(val)
+  if type(val) == type([]):
+    return '[' + ', '.join([ push_str2(i) for i in val ]) + ']'
+  if type(val) == type(True):
+    return str(val)
+  if val == None:
+    return 'None'
+  return val
 
-def eval_case(subname, tipo, code, expected):
-  old_stdout = sys.stdout 
-  new_stdout = io.StringIO() 
-  sys.stdout = new_stdout
+#conversor interno para listas
+def push_str2(val):
+  if type(val) == type(1):
+    return str(val)
+  if type(val) == type(0.5):
+    return str(val)
+  if type(val) == type([]):
+    return '[' + ', '.join([ push_str2(i) for i in val ]) + ']'
+  if type(val) == type(True):
+    return str(val)
+  if val == None:
+    return 'None'
+  return repr(val)
+
+# Reemplaza funciones IO base
+def redirect_io(code):
+  return code.replace('input','new_input').replace('print','new_print')
+
+# Obtener codigo del estudiante desde archivo ipynb
+def get_student_code_from_file(pregunta, raw_student_code):
+  all_cells = raw_student_code
+  for cell in all_cells[::-1]:
+    if cell.strip().startswith(f'# <{pregunta}>') or cell.strip().startswith(f'#<{pregunta}>'):
+      return clean_code(redirect_io(cell))
+  return ''
+
+
+# @timeout(30)
+def ejecutar_codigo(precode, student_code, code):
+  exec(precode)
+  exec(student_code)
+  exec(code)
+
+
+def run_case_with_timeout(precode, student_code, code, timeout):
+  def run_with_timeout():
+    try:
+      exec(precode)
+      exec(student_code)
+      exec(code)
+    except Exception as e:
+      pass
+
+  thread = threading.Thread(target=run_with_timeout)
+  thread.start()
+  thread.join(timeout=timeout)
+
+  if thread.is_alive():
+    thread._stop()
+    return 0
+
+
+def eval_case(case, student_code):
+  global INTERNAL_INPUT_CONTROL
+  global INTERNAL_COUNTER_INPUT
+  global STUDENT_OUTPUT_CONTROL
+  #################################################################
+  INTERNAL_INPUT_CONTROL = case['INPUT'].split(',')
+  INTERNAL_COUNTER_INPUT = 0
+  STUDENT_OUTPUT_CONTROL = ''
+
+  case['OUTPUT'] = case['OUTPUT'].replace('\r', '')
+
   estado = 0
-  comentario = ''
-  try:
-    exec(code)
-    res = sys.stdout.getvalue().strip()
-    sys.stdout = old_stdout
+  estado_msg = 'ERROR'
 
-    if tipo == 'Secreto':
-      res = hashlib.md5( res.encode()).hexdigest() 
-   
-    estado = 1 if res == expected else 0 
+  try:
+    run_case_with_timeout( redirect_io(case['PRECODE']), student_code, redirect_io(case['CODE']), 3 )
+  except Exception as e:
+    print('TimeOut' , case['NAME'], case['TYPE'].upper())
+
+
+  if case['TYPE'].lower() == 'secreto':
+    STUDENT_OUTPUT_CONTROL = hashlib.md5( STUDENT_OUTPUT_CONTROL.strip().encode()).hexdigest()
+    case['OUTPUT'] = case['OUTPUT'].strip()
+
+  estado, estado_msg = [1 ,'CORRECTO'] if STUDENT_OUTPUT_CONTROL == case['OUTPUT'] else [0 , 'INCORRECTO']
+
+  return estado
+
+def eval_test(preg, raw_student_code):
+  try:
+    global SYSTEM_OUTPUT_CONTROL
+    ##############################################################################
+
+    cases = read_cases(get_case_path(preg))
+    assert len(cases) > 0, "Error al cargar los cases.\nFAVOR CONTACTAR A COORDINACION"
+
+    pub = 0
+    t_pub = 0
+    sec = 0
+    t_sec = 0
+    student_code = get_student_code_from_file(preg, raw_student_code)
+    
+    if cases['CASES']:
+      for case in cases['CASES']:
+        if case['TYPE'].lower() == 'publico':
+          pub += eval_case(case, student_code)
+          t_pub += 1
+        if case['TYPE'].lower() == 'secreto':
+          sec += eval_case(case, student_code)
+          t_sec += 1
+
+      return (pub, t_pub, sec, t_sec )
 
   except Exception as e:
-    sys.stdout = old_stdout
-    comentario = 'ERROR ' + str(e) + '\n' 
-
-  return estado, comentario
-
-def get_preg():
-  preg = []
-  with open('cases_urls.txt','r') as f:
-    for l in f.readlines():
-      l = l.strip().split(',')
-      preg.append( l[0] )
-  return preg
-
-def csv_format( data:list):
-  return ','.join([ str(i) for i in data])
+    print(traceback.format_exc())
--- eval_responses.py
+++ eval_responses.py
@@ -1,87 +1,81 @@
-import glob
 
+# LOS ARCHIVOS A EVALUAR DEBEN ESTAR EN EL SIGUIENTE DIRECTORIO
+# ./res/seccion/[ alumno_1.ipynb, alumno_2.ipynb, alumno_3.ipynb ... ]
+global PATH_CASES
+PATH_CASES = 'https://raw.githubusercontent.com/rilopez3/colab_lms/solemne2_2023/cases_urls.txt'
+
+import glob
 from json import load
 from datetime import datetime
+import urllib.request
+import csv
 
-debug = False
-
-protected = ['In', 'Out', 'get_ipython', 'exit', 'quit', 'glob', 'i', 'io', 'sys', 'traceback', 'hashlib',
-             'urllib', 'get_case', 'run_case', 'read_cases', 'run_test', 'clean_exec', 'eval_preg',
-             'get_variables', 'eval_case', 'get_preg', 'protected', 'clean_globals',
-             'load', 'filename', 'fp', 'nb', 'cell', 'cell_code', 'student_code',
-             'data', 'detail_res', 'resume', 'pub', 't_pub', 'sec', 't_sec', 'res_file', 'csv_format', 'debug',
-             'evaluation_variables', 'header','filepath','datetime','globales']
-
-exec(open('colab_lms.py').read())
 exec(open('colab_lms_eval.py').read())
 
-with open('res/results '+ (str(datetime.now()).replace('-','_').replace(':','')[:15]) +'.csv', 'w') as res_file:
-    header = ['path', 'filename'] + evaluation_variables() + ['pub_ok', 'pub_total', 'sec_ok', 'sec_total']
-    print(csv_format(header), file=res_file)
+cases = {}
+f = urllib.request.urlopen(PATH_CASES)
+for l in f.readlines():
+  l = l.decode("utf-8")
+  l = l.strip().split(',')
+  cases[l[0]] = l[1]
+
+
+protected = [
+  'get_ipython', 'exit', 'quit', 'glob', 'i', 'io', 'sys', 'traceback', 'hashlib',
+  'urllib', 'load', 'filename', 'fp', 'nb', 
+  'data', 'csv_format', 'filepath','datetime','globales','cases','PATH_CASES',
+  'get_case_path','protected','header',
+  'read_cases','clean_code','parse_case','new_print','push_str','push_str2','redirect_io','get_student_code_from_file',
+  'eval_case','eval_test','res_file',
+  'threading','run_case_with_timeout','archivos_listos'
+]
+
+
+with open('res/results.csv', 'r') as res_file:
+  csv_reader = csv.reader(res_file)
+  archivos_listos = [row[0] for row in csv_reader]
+
+with open('res/results.csv', 'a') as res_file:
+  if len( archivos_listos) == 0:
+    header = ['path', 'filename','name','seccion'] + [ preg + i for preg in cases.keys() for i in ['_pub','_t_pub','_sec','_t_sec']   ] + ['TOTAL_pub', 'TOTAL_cases_pub', 'total_sec', 'total_cases_sec']    
+    print(csv_format(header), file=res_file, flush=True)
     ##############################################################################
-    for filepath in glob.glob('**/*.ipynb', recursive=True):
 
-        with open(filepath) as fp:
-            nb = load(fp)
-            student_code = ''
-            for cell in nb['cells']:
-                if cell['cell_type'] == 'code':
-                    cell_code = ''.join(line for line in cell['source'])
-                    if '# NO MODIFICAR ESTA CELDA' not in cell_code:
-                        student_code += cell_code + '\n'
-        ############################################################################
-        if debug:
-            print('ARCHIVO', filepath)
-            print('-' * 100)
+  for filepath in glob.glob('**/*.ipynb', recursive=True):
 
-        filename = filepath.split('\\')[-1]
+    if filepath not in archivos_listos:
+      with open(filepath, encoding="utf8") as fp:
+          globales = [i for i in globals().keys() if not i.startswith('_') and not i in protected]
+          for i in globales:
+            exec(i + ' = None')
+          name = ' '.join(filepath.split('\\')[2].split('_')[:2]).upper() 
+          seccion = filepath.split('\\')[1]
 
-        exec(clean_exec())
-        if debug:
-            print('VARIABLES GLOBALES NONEADAS', flush=True)
-        globales = list(globals().keys())
-        for i in globales:
-            if i[0] != '_' and i not in protected:
-                if debug:
-                    print(i)
-                exec(i + ' = None')
+          data = [ filepath, filepath.split('\\')[2], name , seccion ]
+          print(name,'('+seccion+')','->')
+          nb = load(fp)
+          student_code = []
+          final_pub = 0
+          final_total_pub = 0
+          final_sec = 0
+          final_total_sec = 0
+          
+          for cell in nb['cells']:
+              if cell['cell_type'] == 'code':
+                  cell_code = ''.join(line for line in cell['source'])
+                  if '# NO MODIFICAR ESTA CELDA' not in cell_code:
+                    #print(''.join(cell['source']))
+                    student_code.append(''.join(cell['source']))
+          
+          for preg in cases.keys():
+            p, tp,s,ts = eval_test(preg, student_code)
+            final_pub += p
+            final_total_pub += tp
+            final_sec += s
+            final_total_sec += ts
 
-        if debug:
-            print('', flush=True)
-            print('-' * 100, flush=True)
-        ############################################################################
-        exec(student_code)
-        exec(get_variables())
-
-        detail_res = []
-
-        for i in get_preg():
-            detail_res.append([i] + eval_preg(i))
-
-        data['results'] = detail_res
-
-        if debug:
-            print('DETALLE', flush=True)
-            print('-' * 100, flush=True)
-            print(data, flush=True)
-            print('', flush=True)
-            print('-' * 100, flush=True)
-        ############################################################################
-        resume = [filepath, filename] + data['vars']
-        pub = t_pub = sec = t_sec = 0
-        for i in data['results']:
-            pub += i[1]
-            t_pub += i[2]
-            sec += i[3]
-            t_sec += i[4]
-
-        resume += [pub, t_pub, sec, t_sec]
-        if debug:
-            print('TOTAL', flush=True)
-            print('-' * 100, flush=True)
-
-            print(resume)
-            print('', flush=True)
-            print('=' * 100, flush=True)
-            print('', flush=True)
-        print(csv_format(resume), file=res_file)
+            data += [p, tp,s,ts]
+          
+          data += [final_pub,final_total_pub,final_sec,final_total_sec]
+          # print(data)
+          print(csv_format(data), file = res_file, flush=True)
