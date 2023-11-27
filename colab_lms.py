fc9136dfa18338c385bd8bf61a92be187b8ce3b4
Pabloinz <pinzunza@uc.cl>
2023-11-27 12:41:54 -0300

    edit colab_lms

--- colab_lms.py
+++ colab_lms.py
@@ -1,7 +1,7 @@
 import diff_match_patch as dmp_module
 global DEBUG_LOCAL
 DEBUG_LOCAL = False
-PATH_CASES = 'https://raw.githubusercontent.com/rilopez3/colab_lms/solemne2_2023/cases_urls.txt'
+PATH_CASES = 'https://raw.githubusercontent.com/rilopez3/colab_lms/solemne_recuperativa_2023/cases_urls.txt'
 
 ###################################################################################################################################
 import io, sys, traceback, hashlib, urllib.request
